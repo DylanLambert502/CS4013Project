89db41c47536f2f18de75e61f29930d850ce48f6
Conor Harkin <65168529+Conorh2354@users.noreply.github.com>
2021-11-28 17:10:36 +0000

    Add files via upload

--- readReservations.java
+++ readReservations.java
@@ -5,53 +5,71 @@ import java.io.IOException;
 
 
 public class readReservations {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String path = "C:\\Users\\conor\\IdeaProjects\\CS4013Project-main\\CS4013Project-main\\reservations.csv";
         String line = "";
         try {
             BufferedReader br = new BufferedReader(new FileReader(path));
 
-            while((line = br.readLine()) !=null) {
+            while ((line = br.readLine()) != null) {
                 String[] values = line.split(",");
-                System.out.println("The list of names are: " +values[0]);
-                System.out.println("The list of Number are: " +values[1]);
-                System.out.println("The list of Type are: " +values[2]);
-                System.out.println("The list of CheckIn are: " +values[3]);
-                System.out.println("The list of CheckOut are: " +values[4]);
-                System.out.println("The list of AmtOFRooms are: " +values[5]);
-                System.out.println("The list of Roomtype are: " +values[6]);
-                System.out.println("The list of Occupancy are: " +values[7]);
-                System.out.println("The list of Total are: " +values[8]);
 
+                int DD = 0;
+                if (values[6].contains("DD")) {
+                    DD = DD + 1;
+                }
+                int DT = 0;
+                if (values[6].contains("DT")) {
+                    DT = DT + 1;
+                }
+                int DS = 0;
+                if (values[6].contains("DS")) {
+                    DS = DS + 1;
+                }
+                int DF = 0;
+                if (values[6].contains("DF")) {
+                    DF = DF + 1;
+                }
+                int ED = 0;
+                if (values[6].contains("ED")) {
+                    ED = ED + 1;
+                }
+                int ET = 0;
+                if (values[6].contains("ET")) {
+                    ET = ET + 1;
+                }
+                int ES = 0;
+                if (values[6].contains("ES")) {
+                    ES = ES + 1;
+                }
+                int CD = 0;
+                if (values[6].contains("CD")) {
+                    CD = CD + 1;
+                }
+                int CT = 0;
+                if (values[6].contains("DD")) {
+                    CT = CT + 1;
+                }
+                int CS = 0;
+                if (values[6].contains("CS")) {
+                    CS = CS + 1;
+                }
             }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-        e.printStackTrace();
-    }
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
     }
 }
 /*
-Name,       values[0]
-Number,     values[1]
-Type,       values[2]
-CheckIn,    values[3]
-CheckOut,	values[4]
-AmtOFRooms,	values[5]
-Roomtype,	values[6]
-Occupancy,	values[7]
-TotalCost,      values[8]
-
-
-    for( int i=0 , i < valueOf.values[6] , i++ ){
-    if (values[6].equalTo(DD)) {
-    -called room class -
-    DD[0] = DD [0]-1
-    } else if (values[6].equalTo(DT)) {
-    -called room class -
-    DT[0] = DT [0]-1
-    }
-    }
- */
+ System.out.println("The list of names are: " + values[0]);
+         System.out.println("The list of Number are: " + values[1]);
+         System.out.println("The list of Type are: " + values[2]);
+         System.out.println("The list of CheckIn are: " + values[3]);
+         System.out.println("The list of CheckOut are: " + values[4]);
+         System.out.println("The list of AmtOFRooms are: " + values[5]);
+         System.out.println("The list of Roomtype are: " + values[6]);
+         System.out.println("The list of Occupancy are: " + values[7]);
+         System.out.println("The list of Total are: " + values[8]);
 
