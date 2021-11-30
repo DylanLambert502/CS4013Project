82f0e1a859407548ab70a1eab9920ba9081b1dfd
DylanLambert502 <75022302+DylanLambert502@users.noreply.github.com>
2021-11-30 14:13:21 +0000

    Update TextMenu.java

--- TextMenu.java
+++ TextMenu.java
@@ -9,27 +9,32 @@ public class TextMenu {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.printf("Please select menu option:\n" +
-                "1.) Create a reservation\n"+
-                "2.) View reservations\n"+
-                "3.) Test reservation creation\n"+
-                "4.) Exit\n");
+        int choice;
+        while (true) {
+            System.out.printf("Please select menu option:\n" +
+                    "1.) Create a reservation\n" +
+                    "2.) View reservations\n" +
+                    "3.) Test reservation creation\n" +
+                    "4.) Exit\n");
 
-        int userSelection = input.nextInt();
-    do {
-        if (userSelection == 1) {
-            UserReservation.creatRes();
-        } else if (userSelection == 2) {
-            ReadReservations.displayReservations();
-            break;
-        } else if (userSelection == 3) {
-            ReservationTesting.testReservation();
-        } else if (userSelection == 4) {
-            exit(0);
-        } else {
-            System.out.println("Invalid input");
+            choice = input.nextInt();
+
+            switch (choice) {
+                case 1:
+                    UserReservation.creatRes();
+                    break;
+                case 2:
+                    ReadReservations.displayReservations();
+                    break;
+                case 3:
+                    ReservationTesting.testReservation();
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid selection");
+                    break;
+            }
         }
-    } while(true);
-
     }
 }
